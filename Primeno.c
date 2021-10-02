a0242590e4b147795d512f1624780863248bb3b0
Nihal Kumar Das <56214697+Nihal0511@users.noreply.github.com>
2021-10-02 12:24:22 +0530

    Checking whether the number is prime or not.

--- Primeno.c
+++ Primeno.c
@@ -1,21 +1,28 @@
 #include <stdio.h> 
 
-main() {
+int main(void) {
   int n, i, c = 0;
-  printf("Enter any number n:");
+  printf("Enter any integer number n:");
   scanf("%d", &n);
+  if(n<0)
+  {
+    printf("Enter a positive integer value\n");
+  }
+  else
+  {
 
   //logic
   for (i = 1; i <= n; i++) {
       if (n % i == 0) {
          c++;
+        break;
       }
   }
 
-  if (c == 2) {
+  if (c == 0) {
   printf("n is a Prime number");
   }
-  else {
+  if(c==1) {
   printf("n is not a Prime number");
   }
   return 0;    
